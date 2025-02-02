6426047fef2b3f1b2c817cffaa0477516e07e631
ramezmezni16 <ramez.nabila@gmail.com>
2025-02-02 14:51:34 +0100

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="min-h-screen bg-blue-900 flex items-center justify-center">
