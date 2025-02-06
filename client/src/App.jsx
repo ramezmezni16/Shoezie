c90dd35937edd09ce78ee515be2297bdc7958dc2
ramezmezni16 <ramez.nabila@gmail.com>
2025-02-06 13:27:34 +0100

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
       <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </Router>
   );
