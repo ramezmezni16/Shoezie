57059e7b979150c4f97af4cfbda250fe7dfbecff
ramezmezni16 <ramez.nabila@gmail.com>
2025-02-09 14:24:38 +0100

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
+import Home from "./pages/Home";
 
 function App() {
   return (
