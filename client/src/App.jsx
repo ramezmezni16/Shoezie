ecbf81013223402e4c79a715d1caef482b3a164f
ramezmezni16 <ramez.nabila@gmail.com>
2025-02-17 21:52:02 +0100

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CartPage from './pages/CartPage';
+import ProductDetails from './pages/ProductDetails';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
