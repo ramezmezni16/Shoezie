b185b859eba265822f83feea4eef840b70eee52f
ramezmezni16 <ramez.nabila@gmail.com>
2025-02-22 12:09:18 +0100

    sign in design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CartPage from './pages/CartPage';
-import ProductDetails from './pages/ProductDetails';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
-import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Navbar />
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </Router>
+    </>
   );
-}
+};
 
 export default App;
